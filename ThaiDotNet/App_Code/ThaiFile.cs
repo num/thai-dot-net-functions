cf2684d7acc1bf5855c8c4f7369f3254ee04567b
num <dragon_html@hotmail.com>
2010-11-17 23:22:16 +0700

    first code commit

---
+++ ThaiDotNet/App_Code/ThaiFile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ThaiDotNet
+{
+    public static class ThaiFile
+    {
+        public static string Read(string fileName){
+            return String.Empty;
+        }
+    }
+}
